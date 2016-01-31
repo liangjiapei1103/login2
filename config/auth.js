7d2afb774b9d76decc0e56b875fd6e7c3b74492c
Jiapei Liang <liang98@purdue.edu>
2016-01-31 13:42:09 -0500

    Change Callback URL for authentication

--- config/auth.js
+++ config/auth.js
@@ -6,13 +6,13 @@ module.exports = {
 	'facebookAuth' : {
         'clientID'      : '1094347910597809', // your App ID
         'clientSecret'  : '95715f3b7ed3dba3e6d25aa432c04775', // your App Secret
-        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
+        'callbackURL'   : 'https://jiapei-login-system.herokuapp.com/auth/facebook/callback'
     },
 
     'twitterAuth' : {
-        'consumerKey'       : 'your-consumer-key-here',
-        'consumerSecret'    : 'your-client-secret-here',
-        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+        'consumerKey'       : '3dxPqFr6sJbxB1PUOOLnect3i',
+        'consumerSecret'    : 'G4IQkVq3GVUd1XCSkN2UzT1JKKQf1i2dCGkQiNPqw8P4Mj9G9s',
+        'callbackURL'       : 'https://jiapei-login-system.herokuapp.com/auth/twitter/callback'
     },
 
     'googleAuth' : {
