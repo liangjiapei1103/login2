4de5681de0b2871c7cbf65563e8563b28f3494a6
Jiapei Liang <liang98@purdue.edu>
2016-01-31 10:52:21 -0500

    Add facebook authorization

---
+++ config/auth.js
@@ -0,0 +1,24 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+	'facebookAuth' : {
+        'clientID'      : '1094347910597809', // your App ID
+        'clientSecret'  : '95715f3b7ed3dba3e6d25aa432c04775', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : 'your-consumer-key-here',
+        'consumerSecret'    : 'your-client-secret-here',
+        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : 'your-secret-clientID-here',
+        'clientSecret'  : 'your-client-secret-here',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
