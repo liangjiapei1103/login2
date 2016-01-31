b6f9dbe9130990b9b7039519b7b66117d6b1d68d
Jiapei Liang <liang98@purdue.edu>
2016-01-31 11:04:59 -0500

    Add facebook login passport Strategy

--- config/passport.js
+++ config/passport.js
@@ -2,9 +2,13 @@
 
 // load all the things we need
 var LocalStrategy = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 
 // load up the user model
 var User = require('../app/models/user');
+
+// load the auth variables
+var configAuth = require('./auth');
 
 // expose this function to our app using module.exports
 module.exports = function (passport) {
@@ -114,5 +118,60 @@ module.exports = function (passport) {
         	return callback(null, user);
         });
 
+	}));
+
+
+	// Facebook Login =========================================
+
+	passport.use(new FacebookStrategy({
+		// pull in our app id and secret from our auth.js file
+		clientId: configAuth.facebookAuth.clientId,
+		clientSecret: configAuth.facebookAuth.clientSecret,
+		callbackURL: configAuth.facebookAuth.callbackURL,
+		profileFields: ['email', 'displayName', 'photos', 'gender', 'birthday']
+	},
+
+	// facebook will send back the token and profile
+	function (token, refreshToken, profile, callback) {
+
+		// asynchronous
+		process.nextTick(function () {
+
+			// find the user in the database based on their facebook id
+			User.findOne({ 'facebook.id': profile.id }, function (err, user) {
+
+				// if there is an error, stop everything and return that
+				// ie an error connecting to the database
+				if (err) 
+					return callback(err);
+
+				// if the user is found, then log them in
+				if (user) {
+					return callback(null, user); // user found, return that user
+				} else {
+					// if there is no user found with that facebook id, create them
+					var newUser = new User();
+
+					// set all of the facebook information in our user model
+					newUser.facebook.id = profile.id; // set the users facebook id                   
+                    newUser.facebook.token = token; // we will save the token that facebook provides to the user                    
+                    newUser.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
+                    newUser.facebook.name = profile.displayName;
+                    newUser.facebook.picture = profile.photos[0].value;
+                    newUser.facebook.gender = profile.gender;
+
+                    // save our user to the database
+                    newUser.save(function (err) {
+                    	if (err) 
+                    		throw err;
+
+                    	// if successful, return the new user
+                    	return callback(null, newUser);
+                    });
+				}
+
+			});
+
+		});
 	}));
 };
